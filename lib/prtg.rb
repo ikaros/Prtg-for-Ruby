4ca74dcf800df2d931dbb2129dce29a6c12d5fea
kkanello <k@kanello.de>
2011-10-11 10:24:35 +0200

    Change library loading

--- lib/prtg.rb
+++ lib/prtg.rb
@@ -2,6 +2,11 @@
 require "rubygems"
 require "happymapper"
 
+require "prtg/client"
+require "prtg/query"
+require "prtg/sensor"
+require "prtg/utils"
+
 module Prtg
  Dir.glob(File.dirname(__FILE__) + "/lib/**/*").each do |file_path|
    require file_path

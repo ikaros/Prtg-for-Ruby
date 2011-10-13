d4ab097ced0b18ad9bf0a074431060dd3039df95
kkanello <k@kanello.de>
2011-10-13 16:25:11 +0200

    Add a quick example to doc

--- lib/prtg.rb
+++ lib/prtg.rb
@@ -7,6 +7,16 @@ require "prtg/query"
 require "prtg/sensor"
 require "prtg/utils"
 
+# = Description
+# This library is used to communicate with a prtg monitoring instance
+#
+# Simple Example:
+#   http = Net::HTTP.new("subdomain.domain.tld", 443)
+#   http.use_ssl = true
+#   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#
+#   client = Prtg::Client.new(:host => http, :username => "foo", :password => "bar")
+#   puts client.live_data.content(:sensors)
 module Prtg
   VERSION = "0.0.1"
 end
